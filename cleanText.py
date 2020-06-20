ab97a2c23724231419b7a2f086f66f560a040b92
xrachitx <45661647+xrachitx@users.noreply.github.com>
2020-06-20 10:52:36 +0530

    Update cleanText.py

--- cleanText.py
+++ cleanText.py
@@ -10,35 +10,35 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer,PorterStemmer 
 nltk.download('wordnet')
 
-def decodeHTML(data):
-    data = BeautifulSoup(data,"lxml").text
-    return data
+# def decodeHTML(data):
+#     data = BeautifulSoup(data,"lxml").text
+#     return data
 
-def normaliseTextToLower(data):
-    return data.lower()
+# def normaliseTextToLower(data):
+#     return data.lower()
 
-def tokenizeAndPunctuationRemoval(data):
-    tokenizedList=[token for token in RegexpTokenizer('\w+').tokenize(data)]
-    return tokenizedList
+# def tokenizeAndPunctuationRemoval(data):
+#     tokenizedList=[token for token in RegexpTokenizer('\w+').tokenize(data)]
+#     return tokenizedList
 
-def removeNumbers(dataList):
-    dataListRefined = [token for token in dataList if not (token.isnumeric())]
-    return dataListRefined
+# def removeNumbers(dataList):
+#     dataListRefined = [token for token in dataList if not (token.isnumeric())]
+#     return dataListRefined
 
-def removeStopWords(dataList): 
-    dataList = list(set(dataList))
-    stopWords = set(stopwords.words("english"))
-    nonStoppedWords = list(token for token in dataList if token not in stopWords)
-    return nonStoppedWords
+# def removeStopWords(dataList): 
+#     dataList = list(set(dataList))
+#     stopWords = set(stopwords.words("english"))
+#     nonStoppedWords = list(token for token in dataList if token not in stopWords)
+#     return nonStoppedWords
 
-def wordLemmatizer(dataList):
-    lemmatizer = WordNetLemmatizer()
-    lemmatizedText = [lemmatizer.lemmatize(token) for token in dataList]
-    return lemmatizedText
+# def wordLemmatizer(dataList):
+#     lemmatizer = WordNetLemmatizer()
+#     lemmatizedText = [lemmatizer.lemmatize(token) for token in dataList]
+#     return lemmatizedText
 
-def stemming(dataList):
-    stemmer = PorterStemmer()
-    return " ".join([stemmer.stem(token) for token in dataList])
+# def stemming(dataList):
+#     stemmer = PorterStemmer()
+#     return " ".join([stemmer.stem(token) for token in dataList])
 
 
 def cleanData(data):
